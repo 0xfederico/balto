86549922eb55d2c68609012e1237a4c5e4e71179
0xfederico <0xfederico@tutanota.com>
2021-08-29 00:37:31 +0200

    :construction: added homepage and 404 page

--- Configurations/urls.py
+++ Configurations/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from Configurations.views import FourZeroFour, Homepage
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('404-not-found', FourZeroFour.as_view(), name='404-not-found'),
+    path('', Homepage.as_view(), name='homepage'),
 ]
---
+++ Configurations/views.py
@@ -0,0 +1,11 @@
+from django.views.generic import TemplateView
+
+
+class FourZeroFour(TemplateView):
+
+    template_name = '404.html'
+
+
+class Homepage(TemplateView):
+
+    template_name = 'home.html'
